7a0a3411fadce7a980b7a471cb9ba5f13618ecb9
Delicious-PigeonKing <2833818457@qq.com>
2024-07-02 18:36:49 +0800

    Site updated: 2024-07-02 18:36:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/01/21/hello-world/"];function toRandomPost(){
+var posts=["2024/01/21/hello-world/","2023/05/02/双星达成！！！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
